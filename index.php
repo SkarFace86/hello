8e1bf9e8d4b77ad0ccb6b6e8943e3a498969c825
Brandon Skar <bransk01@yahoo.com>
2019-01-07 14:45:28 -0500

    Created a default view

--- index.php
+++ index.php
@@ -15,9 +15,15 @@ require_once('vendor/autoload.php');
 //Create an instance of the Base class
 $f3 = Base::instance();
 
+//Turn of fat free error reporting
+$f3->set('DEBUG', 3);
+
 //Define a default route
 $f3->route('GET /', function() {
-    echo '<h1>Hello world!</h1>';
+    //echo '<h1>Hello world!</h1>';
+
+    $view = new View;
+    echo $view->render('views/home-page.html');
 });
 
 //Run fat-free
